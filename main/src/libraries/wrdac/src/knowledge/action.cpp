4cbc6e4c3bca746b38bf2a1bc4c8dfaa01ce69dc
Tobias Fischer <t.fischer@imperial.ac.uk>
2017-03-17 16:55:55 +0000

    Change website

--- main/src/libraries/wrdac/src/knowledge/action.cpp
+++ main/src/libraries/wrdac/src/knowledge/action.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 2014 WYSIWYD Consortium, European Commission FP7 Project ICT-612139
  * Authors: Ilaria Gori and Maxime Petit
  * email:   ilaria.gori@iit.it, maxime.petit@inserm.fr
- * website: http://https://github.com/robotology/icub-client// 
+ * website: https://github.com/robotology/icub-client/ 
  * Permission is granted to copy, distribute, and/or modify this program
  * under the terms of the GNU General Public License, version 2 or any
  * later version published by the Free Software Foundation.
