c86d4ee149dc67653772a19fbd15b901dad4925b
Phuong Nguyen <ph17dn@gmail.com>
2017-03-24 16:59:29 +0100

    [iol2opc] Add documentation to methods for the 1st time

--- src/modules/iol2opc/include/module.h
+++ src/modules/iol2opc/include/module.h
@@ -247,28 +247,156 @@ protected:
     friend class ClassifierReporter;
 
     void    yInfoGated(const char *msg, ...) const;
+
+    /**
+     * @brief findName Find name of a blob or set it OBJECT_UNKNOWN
+     * @param scores Bottle contains classified results from himrepClassifier, @see classify()
+     * @param tag input string contains blob name, i.e. "blob_<i>" with i as index of a blob in set of results from getBlobs()
+     * @return string value for name of the blob: OBJECT_UNKNOWN or class name from classifier
+     */
     string  findName(const Bottle &scores, const string &tag);
+
+    /**
+     * @brief skimBlobs Remove blobs which are too far away in the Cartesian space wrt Root frame
+     * @param blobs Original Yarp Bottle contains all blobs
+     * @return Result Bottle contains valid blobs
+     */
     Bottle  skimBlobs(const Bottle &blobs);
+
+    /**
+     * @brief thresBBox Constrain a bounding box with respect to an image size
+     * @param bbox CvRect define the bounding box
+     * @param img Image with information used to constrain the bounding box
+     * @return True if the size of the constrained bounding box is large enough (bigger than tracker_min_blob_size)
+     */
     bool    thresBBox(CvRect &bbox, const Image &img);
+
+    /**
+     * @brief getBlobs Wrapper to get blobs information from the blobExtractor port
+     * @return Yarp Bottle contains all valid segmented object blobs from lbpExtract module
+     */
     Bottle  getBlobs();
+
+    /**
+     * @brief getBlobCOG Get the blob center of the object defined by i
+     * @param blobs Yarp Bottle contains information of the estimated object bounding boxes: top left x, y, bottom right x, y
+     * @param i integer value to define object
+     * @return A CvPoint containing x, y coordinate of the blob center
+     */
     CvPoint getBlobCOG(const Bottle &blobs, const int i);
+
+    /**
+     * @brief get3DPosition Get the 3D point coordinate in Root frame through SFM
+     * @param point A CvPoint for the estimated coordinate of an image pixel.
+     * @param x Vector for 3D point coordinate. If SFM return more than one 3D point as result, the coordinate is average of all result
+     * @return True if can get 3D point from SFM, False otherwise
+     */
     bool    get3DPosition(const CvPoint &point, Vector &x);
+
+    /**
+     * @brief get3DPositionAndDimensions Get the 3D point coordinate and dimension in Root frame through SFM
+     * @param bbox A CvRect for bounding box of object
+     * @param x Vector for 3D point coordinate. If SFM return more than one 3D point as result, the coordinate is average of all result
+     * @param dim Vector for 3D dimensions of object, calculated using coordinates of points from SFM
+     * @return True if get response from SFM, False otherwise
+     */
     bool    get3DPositionAndDimensions(const CvRect &bbox, Vector &x, Vector &dim);
     Vector  calibPosition(const Vector &x);
+
+    /**
+     * @brief getClickPosition Get the position on image that user clicks on
+     * @param pos A CvPoint contains 2D coordinate of the estimated pixel that user clicks on
+     * @return True if the result pixel is valid, False otherwise
+     */
     bool    getClickPosition(CvPoint &pos);
+
+    /**
+     * @brief acquireImage Get calibrated image from the left camera of iCub
+     */
     void    acquireImage();
+
+    /**
+     * @brief drawBlobs Add bounding box and name of object in output image shown through port imgRtLocOut
+     * @param blobs Yarp Bottle contains information of the estimated object bounding boxes: top left x, y, bottom right x, y
+     * @param i integer value as index of the blob to draw the bounding box
+     * @param scores Bottle contains classified results from himrepClassifier, @see classify()
+     */
     void    drawBlobs(const Bottle &blobs, const int i, const Bottle &scores);
+
+    /**
+     * @brief rotate Create new OpenCV matrix by rotating an original one an angle in 2D, a wrapper of cv::warpAffine
+     * @param src Original OpenCV matrix
+     * @param angle Double value for rotated angle
+     * @param dst New rotated OpenCV matrix
+     */
     void    rotate(cv::Mat &src, const double angle, cv::Mat &dst);
+
+    /**
+     * @brief drawScoresHistogram Draw histogram of objects' scores
+     * @param blobs Yarp Bottle contains information of the estimated object bounding boxes: top left x, y, bottom right x, y
+     * @param i integer value as index of the blob to draw the bounding box
+     * @param scores Bottle contains classified results from himrepClassifier, @see classify()
+     */
     void    drawScoresHistogram(const Bottle &blobs, const Bottle &scores, const int i);
+
+    /**
+     * @brief findClosestBlob Find the closest blob to location
+     * @param blobs Yarp Bottle contains information of the estimated object bounding box: top left x, y, bottom right x, y
+     * @param loc A CvPoint contains 2D coordinate of pixel expressed the location
+     * @return integer value of the corresponding blob to the location
+     */
     int     findClosestBlob(const Bottle &blobs, const CvPoint &loc);
+
+    /**
+     * @brief findClosestBlob Find the closest blob to location
+     * @param blobs Yarp Bottle contains information of the estimated object bounding boxes: top left x, y, bottom right x, y
+     * @param loc A Vector contains 3D coordinate of the location in the Root frame
+     * @return integer value of the corresponding blob to the location
+     */
     int     findClosestBlob(const Bottle &blobs, const Vector &loc);
+
+    /**
+     * @brief classify Classify an object using himrepClassifier
+     * @param blobs Yarp Bottle contains information of the estimated object bounding boxes: top left x, y, bottom right x, y. This will be sent to himrepClassifier for object detection
+     * @return Yarp Bottle contains class of object and confidence score
+     */
     Bottle  classify(const Bottle &blobs);
+
+    /**
+     * @brief train Training himrepClassifier by sending an object by name and its bounding box
+     * @param object string value contains name of object to train
+     * @param blobs Yarp Bottle contains information of the estimated object bounding boxes: top left x, y, bottom right x, y
+     * @param i integer value for the corresponding blob of the object needed to train
+     */
     void    train(const string &object, const Bottle &blobs, const int i);
+
+    /**
+     * @brief doLocalization The whole procedure to classify objects, from getting image to draw bounding boxes arround objects
+     */
     void    doLocalization();
+
+    /**
+     * @brief updateOPC The whole procedure to update OPC, including naming known and unknown objects, drawing bounding boxes around objects
+     */
     void    updateOPC();
+
+    /**
+     * @brief getReachableArea Analyse if an object is inside which region: Human, Shared, Robot or Not-Reachable
+     * @param objpos Vector of object position
+     * @return ObjectArea that the object belongs to
+     */
     ObjectArea getReachableArea(const yarp::sig::Vector &objpos);
 
     bool    configure(ResourceFinder &rf);
+
+    /**
+     * @brief setBounds Set min & max for a vector
+     * @param rf ResourceFinder object
+     * @param bounds Vector to set bounds to
+     * @param configName string value for name of the parameter in config file
+     * @param std_lower double value for min bound
+     * @param std_upper double value for max bound
+     */
     void    setBounds(ResourceFinder &rf, Vector &bounds, string configName, double std_lower, double std_upper);
     bool    interruptModule();
     bool    close();
