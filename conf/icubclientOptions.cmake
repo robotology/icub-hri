e712e63e0943eb3c1fb1317d7237acd31feb5a69
Tobias Fischer <t.fischer@imperial.ac.uk>
2017-04-13 10:39:03 +0100

    Require C++11 (no more C++0x)

--- conf/icubclientOptions.cmake
+++ conf/icubclientOptions.cmake
@@ -23,15 +23,12 @@ elseif(CMAKE_COMPILER_IS_GNUCXX OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 
    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${WARNINGS_STRING}")
 
+   # require C++11
    include(CheckCXXCompilerFlag)
    CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
-   CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
    if(COMPILER_SUPPORTS_CXX11)
        message(STATUS "Enabled C++11 support for compiler ${CMAKE_CXX_COMPILER}.")
        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-   elseif(COMPILER_SUPPORTS_CXX0X)
-       message(STATUS "Enabled C++0x support for compiler ${CMAKE_CXX_COMPILER}.")
-       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
    else()
        message(SEND_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler, and welcome to the modern world.")
    endif()
@@ -41,9 +38,8 @@ endif()
 if(NOT CMAKE_CONFIGURATION_TYPES)
     if(NOT CMAKE_BUILD_TYPE)
        set(CMAKE_BUILD_TYPE "Release" CACHE STRING
-           "Choose the build type, recommanded options are: Debug or Release" FORCE)
+           "Choose the build type, recommended options are: Debug or Release" FORCE)
     endif()
     set(ICUBCLIENT_BUILD_TYPES "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
     set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ${ICUBCLIENT_BUILD_TYPES})
 endif()
-
