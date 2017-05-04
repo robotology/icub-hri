3273745bfd5ea57ac3a122f94ccc0484df3b9735
Tobias-Fischer <t.fischer@imperial.ac.uk>
2017-05-04 15:50:41 +0100

    Test #2

--- doxygen/deploy-to-gh-pages.sh
+++ doxygen/deploy-to-gh-pages.sh
@@ -10,6 +10,6 @@ cd doxygen
 bash ./doc-compile.sh
 git add -f doc
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER provided doxygen documentation"
-git remote add origin-pages https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git > /dev/null 2>&1
+git remote add origin-pages https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git > /dev/null 2>&1
 git push --force --quiet --set-upstream origin-pages gh-pages > /dev/null 2>&1
 
