4592422dad5ec6b059d527e6d48454321561ea91
Tobias-Fischer <t.fischer@imperial.ac.uk>
2017-05-04 14:10:24 +0100

    Try new deploy method

--- doxygen/deploy-to-gh-pages.sh
+++ doxygen/deploy-to-gh-pages.sh
@@ -5,12 +5,11 @@ set -o errexit
 git config --global user.email "t.fischer@imperial.ac.uk"
 git config --global user.name "Tobias-Fischer"
 
-git checkout gh-pages
-git rebase master
+git checkout -b gh-pages
 cd doxygen
 bash ./doc-compile.sh
-git add -u .
-GIT_COMMITTER_DATE="`date`" git commit --amend --no-edit --date "`date`"
-git push --force-with-lease --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git add -f doc
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER provided doxygen documentation"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
 git checkout master
 
