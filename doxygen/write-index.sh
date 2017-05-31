1fea761533d3d407ceed96d4f77b7d378c78a256
Tobias Fischer <t.fischer@imperial.ac.uk>
2017-05-31 16:23:51 +0100

    Update write-index.sh

--- doxygen/write-index.sh
+++ doxygen/write-index.sh
@@ -7,14 +7,14 @@ FILE="../index.html"
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1;url=https://robotology.github.io/icub-client/doxygen/doc/html/index.html">
+        <meta http-equiv="refresh" content="1;url=https://robotology.github.io/icub-hri/doxygen/doc/html/index.html">
         <script type="text/javascript">
-            window.location.href = "https://robotology.github.io/icub-client/doxygen/doc/html/index.html"
+            window.location.href = "https://robotology.github.io/icub-hri/doxygen/doc/html/index.html"
         </script>
         <title>Page Redirection</title>
     </head>
     <body>
-        If you are not redirected automatically, follow the <a href="https://robotology.github.io/icub-client/doxygen/doc/html/index.html">link to the documentation</a>
+        If you are not redirected automatically, follow the <a href="https://robotology.github.io/icub-hri/doxygen/doc/html/index.html">link to the documentation</a>
     </body>
 </html>
 EOM
